0b7e802ddd83b0ac367cf2b068dc92ead9acd9e3
Ram Keerthy Athinarayanan <rkeerthya@gmail.com>
2019-12-03 15:43:35 -0500

    initial2

---
+++ src/MathematicalOperations.java
@@ -0,0 +1,25 @@
+
+public class MathematicalOperations {
+	public static int log(int x, int b) {
+		return (int) (Math.log(x) / Math.log(b));
+	}
+	
+	public static double logDouble(int x, int b) {
+		return (Math.log(x) / Math.log(b));
+	}
+	
+	public static boolean isInteger(double x) {
+		if (x == (int)x)
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static boolean checkLogIsInteger(int x, int b) {
+		double logValue = (Math.log(x) / Math.log(b));
+		return isInteger(logValue);
+	}
+}
